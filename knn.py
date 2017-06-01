07ad78ad9ec736558ad004d73d0e1d603c6ee0c7
NichDullam <npdullam@gmail.com>
2017-06-01 09:09:51 -0400

    Cleaning 1

--- knn.py
+++ knn.py
@@ -2,8 +2,6 @@ import csv
 import math
 import operator
 
-
-
 #CLASS START
 class feedTesting:
         
@@ -13,87 +11,92 @@ class feedTesting:
         def __init__(self, testInstance):
                 self.testInstance = testInstance
 
-
-
 #STATIC METHODS
 
         #sqr(sums of (differences squared))        
         @staticmethod
         def euclideanDistance(trainingInstance, testInstance, length):
-           distance = 0
-           for x in range(length):
-               distance += pow((trainingInstance[x] - testInstance[x]), 2)
-           if distance == 0:
-                   return 1
-           else:
-                   return 1- (math.sqrt(distance) *.1)
+                distance = 0
+                for i in range(length):
+                        distance += pow((trainingInstance[i] - testInstance[i]), 2)
+                return 1- (math.sqrt(distance) *.1)
+
+        """
+        loadDataSet:
+        Converts filename.data into a
+        usable trainingSet array
+        """
         
-
-
-
         def loadDataSet(self, dataValTotal, filename, trainingSet=[]):
                 with open(filename, 'rb') as csvfile:
-                    lines = csv.reader(csvfile)
-                    dataset = list(lines)
-                    for x in range(len(dataset)-1):
-                        #change the range value depending on the amount of variables being considered
-                       for y in range(dataValTotal):
-                           dataset[x][y] = float(dataset[x][y])
-                       trainingSet.append(dataset[x])
+                        lines = csv.reader(csvfile)
+                        dataset = list(lines)
+                        for i in range(len(dataset)-1):
+                                #change the range value depending on the amount of variables being considered
+                                for j in range(dataValTotal):
+                                        dataset[i][j] = float(dataset[i][j])
+                                        trainingSet.append(dataset[i])
                 self.training = trainingSet
                        
-
+        """
+        Finds neighbors(under the k value) with the testInstance
+        provided via the constructor. First edge removes (pops)
+        the given index value from the distances array. When the
+        second edge arrives, a method of index recording/ignoring
+        is used due to inefficiencies with index popping.
+        """
 
         def findNeighbors(self, k):
-            distances = []
-            for x in range(len(self.training)):
-                arrIndex = x
-                aff = feedTesting.euclideanDistance(self.training[x], self.testInstance, len(self.testInstance)-1)
-                distances.append((self.training[x], aff))
+                distances = []
+                for x in range(len(self.training)):
+                        arrIndex = x
+                        aff = feedTesting.euclideanDistance(self.training[x], self.testInstance, len(self.testInstance)-1)
+                        distances.append((self.training[x], aff))
 
-            distances.sort(key=operator.itemgetter(1), reverse = True)
-            neighbors = [[]]
+                distances.sort(key=operator.itemgetter(1), reverse = True)
+                neighbors = [[]]
 
-            for x in range(k):
-                neighbors[0].append(distances[0])
-                distances.pop(0)
+                for range(k):
+                        neighbors[0].append(distances[0])
+                        distances.pop(0)
         
-            #Finds the "2nd layer" of neighbors (1st edge)
-            lengthNeigh = len(neighbors)
+                #2nd Edge
+                tempDist = []
+                for i in range(len(neighbors[0])):
+                #Removes the previously declared neighbor values before
+                        for y in range(len(distances)):
+                                #All distances for both neighbors are saved to the same array, just organized with an additional X value alongside their original Y index
+                                localAff = feedTesting.euclideanDistance(distances[j][0], neighbors[0][i][0], len(self.testInstance)-1)
+                                completeAff = neighbors[0][i][1] * localAff
+                        
+                                #replaces the previously declared distance variables from the first edge
+                                if x == 0:
+                                        distances[j] = (distances[j][0], localAff, completeAff, i, j)
+                                #appends to the distances array as the second round of neighboring
+                                else:
+                                        distances.append((distances[j][0], localAff, completeAff, i, j))
+        
+                distances.sort(key=operator.itemgetter(1), reverse = True)
 
-            tempDist = []
-            for x in range(len(neighbors[0])):
-               #Removes the previously declared neighbor values before
-               for y in range(len(distances)):
-                    #distUser = feedTesting.euclideanDistance(self.training[y], self.testInstance, len(self.testInstance)-1)
-                    localAff = feedTesting.euclideanDistance(distances[y][0], neighbors[0][x][0], len(self.testInstance)-1)
-                    completeAff = neighbors[0][x][1] * localAff
+                #yIndex marks all already used distance values (reduces overlap)   
+                yIndex = []      
+                for i in range(len(neighbors[0])):
+                        count = 0
+                        index = 0
 
-                    if x == 0:
-                            distances[y] = (distances[y][0], localAff, completeAff, x, y)
-                    else:
-                            distances.append((distances[y][0], localAff, completeAff, x, y))
-
-            distances.sort(key=operator.itemgetter(1), reverse = True)
-               
-            yIndex = []      
-            for x in range(len(neighbors[0])):   
-                    count = 0
-                    index = 0
-
-                    while count < k:
-                        cont = True 
-                        for i in range(len(yIndex)):
-                                if distances[index][4] == yIndex[i]:
-                                        cont = False
-                        if distances[index][3] == x and cont == True:
-                                tempDist.append(distances[index])
-                                yIndex.append(distances[index][4])
+                        while count < k:
+                                cont = True 
+                                for j in range(len(yIndex)):
+                                        if distances[index][4] == yIndex[j]:
+                                                cont = False
+                                if distances[index][3] == i and cont == True:
+                                        tempDist.append(distances[index])
+                                        yIndex.append(distances[index][4])
                                 
-                                count += 1
-                        index += 1
-                                                        
-            neighbors.append((tempDist))
+                                        count += 1
+                                index += 1
+
+                neighbors.append((tempDist))
 
             return neighbors
         
--- preferenceConversions.py
+++ preferenceConversions.py
@@ -2,9 +2,6 @@ import csv
 import math
 import json
 
-
-
-
 #CLASS START
 class prefConv:
 
@@ -17,8 +14,6 @@ class prefConv:
         prefConv.collections = collections
         prefConv.categories = categories
 
-
-
 #STATIC METHODS
 
     #SUBJECT TO INEFFICENCIES
@@ -28,20 +23,20 @@ class prefConv:
         externality = 0
 
         #adds all of the variables within the given collection to the collectionVariables array
-        for x in range(len(collections)):
-            for y in range(1, len(collections[x])):
-                if collections[x][0] == collectionTitle:
-                    for j in range(len(binaryValues)):
-                        if binaryValues[j][0] == collections[x][y][0] and binaryValues[j][1] == 1:
-                            collectionVariables.append(collections[x][y][0])
+        for i in range(len(collections)):
+            for j in range(1, len(collections[x])):
+                if collections[i][0] == collectionTitle:
+                    for k in range(len(binaryValues)):
+                        if binaryValues[k][0] == collections[i][j][0] and binaryValues[k][1] == 1:
+                            collectionVariables.append(collections[i][j][0])
     
         #begins the determinance of the externality quantities (future examinance of the process necessary)
-        for x in range(len(collections)):
-            for y in range(1, len(collections[x])):
-                tempTitle = collections[x][y][0]
-                for j in range(len(collectionVariables)):
-                    if collections[x][0] != collectionTitle and collectionVariables[j] == collections[x][y][0]:
-                        externality += collections[x][y][1]
+        for i in range(len(collections)):
+            for j in range(1, len(collections[i])):
+                tempTitle = collections[i][y][0]
+                for k in range(len(collectionVariables)):
+                    if collections[i][0] != collectionTitle and collectionVariables[k] == collections[i][j][0]:
+                        externality += collections[i][j][1]
         
         return math.sqrt(externality * .15)
 
@@ -73,23 +68,21 @@ class prefConv:
         return binValues
 
 
-
     def weightsToVals(self, binValues):
         values = []
 
         #SUBJECT TO INEFFINCIES
-        for x in range(len(self.collections)):
+        for i in range(len(self.collections)):
             weight = 0
-            for y in range(len(self.collections[x])-1):
+            for j in range(1, len(self.collections[x])-1):
                 found = False
-                pos = 1 + y
                 index = 0
 
             
-                for i in range(len(binValues)):
-                    if self.collections[x][pos][0] == binValues[i][0]:
+                for k in range(len(binValues)):
+                    if self.collections[i][j][0] == binValues[k][0]:
                         index = i
-                weight += self.collections[x][pos][1] * binValues[index][1]
-                weight += prefConv.findExternalities(self.collections, self.collections[x][0], binValues)
-            values.append((self.collections[x][0], weight))
+                weight += self.collections[i][j][1] * binValues[index][1]
+                weight += prefConv.findExternalities(self.collections, self.collections[i][0], binValues)
+            values.append((self.collections[i][0], weight))
         return values
